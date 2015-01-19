ddf0a0b4b327350a5f0a8b31443109490c7fe840
Yi Guo <yi.guo@yahoo.com>
2015-01-18 21:21:03 -0500

    Add comments for each function

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import codecs
 import sys
 import os
 
-def get_md5(path):
+# Return the MD5 checksum of the specified file.
+def getMd5(path):
     with open(path, 'rb') as file:
         md5 = hashlib.md5()
         while True:
@@ -13,25 +14,21 @@ def get_md5(path):
             md5.update(content)
     return md5.hexdigest()    
 
-def is_empty(path):
+# Return true if the specified path is empty.
+def isEmpty(path):
     for root, directories, files in os.walk(path):
         if files:
             return False
-        return all(is_empty(root + '\\' + directory) for directory in directories)
+        return all(isEmpty(root + '\\' + directory) for directory in directories)
     return True
 
-def empty_dir(path):
-    for root, directories, files in os.walk(path):
-        if is_empty(root):
-            print(root)
-
+# On default, return the MD5 checksums for the specified file(s) and output the result in a text file.
 def main():
     output = codecs.open('result.txt', 'a', 'utf-8')
     for root, _, files in os.walk(sys.argv[1]):
         for file in files:
             path = root + '\\' + file
-            print(path)
-            output.write('%s\t%s\n' % (path, get_md5(path)))
+            output.write('%s\t%s\n' % (path, getMd5(path)))
     output.close()
 
 main()
